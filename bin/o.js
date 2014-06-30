e43398f18595dd474cbfb8ca75e5036cb8604788
Will Shulman <will@objectlabs.com>
2014-06-29 21:42:26 -0700

    adding support for cmdline args

--- bin/o.js
+++ bin/o.js
@@ -4,7 +4,24 @@ if (!path) {
    return
 }
 
+require.main.__mainComponentPath = path
 var result = require('maker')._o(module)(path)
+if (result._init) {
+    result._init(process.argv.slice(3))
+}
+
 console.log(result)
 
+// yargs
+// can print usage (we should gen)
+// can give example (gen?)
+// describe (can desribe opts)
+// can require opts
+// can set defaults
+// can require num args
+// can mark as boolean
+// can alias
+// long and short
+// count -vvv
+// 
 
